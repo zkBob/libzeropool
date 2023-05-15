57438e22fa499ad294e2fd5d1d0b1cae7c9b4d48
Alexander Filippov <aleksander.fill@gmail.com>
2023-05-15 14:17:55 +0300

    Fix typo

--- tests/encryption.rs
+++ tests/encryption.rs
@@ -16,7 +16,7 @@ use libzeropool_zkbob::fawkes_crypto::engines::bn256::Fr;
 fn test_encryption() {
     let mut rng = thread_rng();
     let sender_eta = rng.gen();
-    let seder_kappa = derive_key_kappa(sender_eta);
+    let sender_kappa = derive_key_kappa(sender_eta);
     let receiver_eta = rng.gen();
 
 
@@ -28,9 +28,9 @@ fn test_encryption() {
     note[0].p_d = derive_key_p_d(note[0].d.as_num().clone(), receiver_eta, &*POOL_PARAMS).x;
     
 
-    let ciphertext = cipher::encrypt(&(0..32).map(|_| rng.gen()).collect::<Vec<_>>(), &seder_kappa, account, &note, &*POOL_PARAMS);
+    let ciphertext = cipher::encrypt(&(0..32).map(|_| rng.gen()).collect::<Vec<_>>(), &sender_kappa, account, &note, &*POOL_PARAMS);
 
-    let result_out = cipher::decrypt_out(sender_eta, &seder_kappa, &ciphertext, &*POOL_PARAMS);
+    let result_out = cipher::decrypt_out(sender_eta, &sender_kappa, &ciphertext, &*POOL_PARAMS);
 
     assert!(result_out.is_some(), "Could not decrypt outgoing data.");
         let (account_out, note_out) = result_out.unwrap();
